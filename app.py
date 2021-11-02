102b20c157d1471257fca2d92ff59ef89b7f36d9
Minal Chhatre <sumanchhatre01@gmail.com>
2021-11-01 19:55:42 -0700

    hosting on pythonanywhere

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir,"mydatabase.db"))
+database_file = "sqlite:///{}".format(
+    os.path.join(project_dir, "mydatabase.db"))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
-
-
 
 
 class Expense(db.Model):
@@ -19,10 +18,10 @@ class Expense(db.Model):
     category = db.Column(db.String(50), nullable=False)
 
 
-
 @app.route('/')
 def add():
     return render_template('add.html')
+
 
 @app.route("/delete/<int:id>")
 def delete(id):
@@ -31,12 +30,14 @@ def delete(id):
     db.session.commit()
     return redirect("/expenses")
 
+
 @app.route("/updateexpense/<int:id>")
 def updateexpense(id):
     expense = Expense.query.filter_by(id=id).first()
-    return render_template("updateexpense.html",expense=expense)
+    return render_template("updateexpense.html", expense=expense)
 
-@app.route('/edit',methods=['POST'])
+
+@app.route('/edit', methods=['POST'])
 def edit():
     id = request.form["id"]
     date = request.form["date"]
@@ -52,8 +53,6 @@ def edit():
 
     db.session.commit()
     return redirect("/expenses")
-
-   
 
 
 @app.route("/expenses")
@@ -74,23 +73,26 @@ def expenses():
             t_food += expense.amount
         elif expense.category == "entertainment":
             t_entertainment += expense.amount
-    return render_template("expenses.html",expenses=expenses, total=total, t_business=t_business,t_entertainment=t_entertainment,t_food=t_food,t_other=t_other)    
+    return render_template("expenses.html", expenses=expenses, total=total, t_business=t_business, t_entertainment=t_entertainment, t_food=t_food, t_other=t_other)
 
-@app.route('/addexpense',methods=['POST'])
+
+@app.route('/addexpense', methods=['POST'])
 def addexpense():
     date = request.form['date']
-    expensename = request.form['expensename'] 
+    expensename = request.form['expensename']
     amount = request.form['amount']
     category = request.form['category']
     print(date + "" + expensename + "" + amount + "" + category)
-    expense = Expense(date=date,expensename=expensename,amount=amount,category=category)
+    expense = Expense(date=date, expensename=expensename,
+                      amount=amount, category=category)
     db.session.add(expense)
     db.session.commit()
     return redirect('/expenses')
 
-@app.route('/addview',methods=['GET','POST'])
+
+@app.route('/addview', methods=['GET', 'POST'])
 def addview():
-    if request.method =='GET':
+    if request.method == 'GET':
         expenses = Expense.query.all()
         total = 0
         t_business = 0
@@ -107,20 +109,20 @@ def addview():
                 t_food += expense.amount
             elif expense.category == "entertainment":
                 t_entertainment += expense.amount
-    elif request.method=="POST":
+    elif request.method == "POST":
         date = request.form['date']
-        expensename = request.form['expensename'] 
+        expensename = request.form['expensename']
         amount = request.form['amount']
         category = request.form['category']
         print(date + "" + expensename + "" + amount + "" + category)
-        expense = Expense(date=date,expensename=expensename,amount=amount,category=category)
+        expense = Expense(date=date, expensename=expensename,
+                          amount=amount, category=category)
         db.session.add(expense)
         db.session.commit()
         return redirect('/addview')
-    return render_template("addview.html",expenses=expenses, total=total, t_business=t_business,t_entertainment=t_entertainment,t_food=t_food,t_other=t_other)
+    return render_template("addview.html", expenses=expenses, total=total, t_business=t_business, t_entertainment=t_entertainment, t_food=t_food, t_other=t_other)
 
 
+if __name__ == '__main__':
 
-if __name__ =='__main__':
-    
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=True)
